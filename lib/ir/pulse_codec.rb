6661da89778216a1df3874ed6b5f7308230b5b11
Yuji Nakayama <nkymyj@gmail.com>
2014-08-03 15:06:57 +0900

    Add PulseCodec.require_all

--- lib/ir/pulse_codec.rb
+++ lib/ir/pulse_codec.rb
@@ -1,5 +1,12 @@
 module IR
   module PulseCodec
+    def self.require_all
+      pattern = File.join(File.dirname(__FILE__), 'pulse_codec', '*.rb')
+      Dir.glob(pattern) do |path|
+        require path
+      end
+    end
+
     def self.all
       @all ||= []
     end
